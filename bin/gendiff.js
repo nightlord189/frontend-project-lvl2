3ae77959b7926a16df04f2cdf71e0cef4e1bdc96
Alexey Buravov <night.lord.189@gmail.com>
2020-07-29 00:18:24 +0600

    step 2: help: add usage, add --format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 program.version('0.0.1');
 program
     .description('Compares two configuration files and shows a difference.')
+    .usage ('[options] <filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
 
