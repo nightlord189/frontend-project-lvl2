18181eaf39de5bc9b33e84119046d1b5488d83d2
Alexey Buravov <night.lord.189@gmail.com>
2020-08-05 22:45:35 +0600

    Test extend

--- __tests__/single.test.js
+++ __tests__/single.test.js
@@ -1,5 +1,7 @@
 import compareSingle from '../src/single.js';
 
 test ('single', ()=> {
-    console.log(dd);
+    console.log('dd');
+    const d = compareSingle('file1.json', 'file2.json');
+    expect(3).toEqual(3);
 })
