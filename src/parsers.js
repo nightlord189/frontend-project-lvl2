ce1da7764fd7790e62b079db0d698a7b17e17e33
Alexey Buravov <night.lord.189@gmail.com>
2020-08-15 03:29:38 +0600

    YAML flat compare

--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,13 @@
+import yaml from 'js-yaml';
 
 const parse = (rawData, extension) => {
     switch (extension) {
         case 'json':
             return JSON.parse(rawData);
+        case 'yml':
+        case 'yaml':
+            const parsed = yaml.safeLoad(rawData);
+            return parsed;
         default:
             return rawData;
     }
