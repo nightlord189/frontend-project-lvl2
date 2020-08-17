01b4548bf4b7dd9837b9e5e6926c8f970b4e1443
Alexey Buravov <night.lord.189@gmail.com>
2020-08-18 01:40:27 +0600

    CompareMapKey remove. Lint fix

--- src/nested.js
+++ src/nested.js
@@ -14,85 +14,43 @@ import getAllKeys from './utils.js';
 }
 */
 
-const compareMapKey = (item, data1, data2) => {
-  if (!_.has(data2, item.key)) {
-    return {
-      key: item.key,
-      status: 'remove',
-      oldValue: _.isObject(data1[item.key]) ? compare(data1[item.key], data1[item.key], item.depth) : data1[item.key],
-      depth: item.depth,
-    };
-  }
-  if (!_.has(data1, item.key)) {
-    return {
-      key: item.key,
-      status: 'add',
-      newValue: _.isObject(data2[item.key]) ? compare(data2[item.key], data2[item.key], item.depth) : data2[item.key],
-      depth: item.depth,
-    };
-  }
-  if (_.isEqual(data1[item.key], data2[item.key])) {
-    return {
-      key: item.key,
-      status: 'same',
-      oldValue: _.isObject(data1[item.key]) ? compare(data1[item.key], data2[item.key], item.depth) : data1[item.key],
-      depth: item.depth,
-    };
-  }
-  if (_.isObject(data1[item.key]) && _.isObject(data2[item.key])) {
-    return {
-      key: item.key,
-      status: 'change',
-      newValue: compare(data1[item.key], data2[item.key], item.depth),
-      depth: item.depth,
-    };
-  }
-  return {
-    key: item.key,
-    status: 'change',
-    oldValue: _.isObject(data1[item.key]) ? compare(data1[item.key], data1[item.key], item.depth) : data1[item.key],
-    newValue: _.isObject(data2[item.key]) ? compare(data2[item.key], data2[item.key], item.depth) : data2[item.key],
-    depth: item.depth,
-  };
-};
-
 const compare = (data1, data2) => {
   const keys = getAllKeys(data1, data2);
-  const resultObj = keys.map((key)=> {
+  const resultObj = keys.map((key) => {
     if (!_.has(data2, key)) {
       return {
         key,
         status: 'remove',
-        value: data1[key]
+        value: data1[key],
       };
     }
     if (!_.has(data1, key)) {
       return {
         key,
         status: 'add',
-        value: data2[key]
+        value: data2[key],
       };
     }
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
       return {
         key,
         status: 'nested',
-        children: compare(data1[key], data2[key])
+        children: compare(data1[key], data2[key]),
       };
     }
     if (_.isEqual(data1[key], data2[key])) {
       return {
         key,
         status: 'same',
-        value: data1[key]
+        value: data1[key],
       };
-    } 
+    }
     return {
       key,
       status: 'change',
       value: data1[key],
-      valueNew: data2[key]
-    };               
+      valueNew: data2[key],
+    };
   });
   return resultObj;
 };
