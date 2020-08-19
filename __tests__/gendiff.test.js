ce382c31f1e998aa86e593ffd6bff4ec473a327f
Alexey Buravov <night.lord.189@gmail.com>
2020-08-20 04:21:07 +0600

    Test change

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,19 +4,6 @@ import compareNested from '../src/nested.js';
 
 const fixturePath = '__tests__/__fixtures__';
 
-test('flat_json', () => {
-  let compare = compareNested(`${fixturePath}/file1.json`, `${fixturePath}/file2.json`);
-  let result = fs.readFileSync(`${fixturePath}/result1.txt`, { encoding: 'utf8', flag: 'r' });
-  expect(compare).toEqual(result);
-
-  compare = compareNested(`${fixturePath}/empty.json`, `${fixturePath}/empty.json`);
-  expect(compare.split(/\r?\n/)).toHaveLength(3);
-
-  compare = compareNested(`${fixturePath}/empty.json`, `${fixturePath}/file2.json`);
-  result = fs.readFileSync(`${fixturePath}/result2.txt`, { encoding: 'utf8', flag: 'r' });
-  expect(compare).toEqual(result);
-});
-
 test('flat_yaml', () => {
   const compare = compareNested(`${fixturePath}/file1.yml`, `${fixturePath}/file2.yml`);
   const result = fs.readFileSync(`${fixturePath}/result1.txt`, { encoding: 'utf8', flag: 'r' });
@@ -30,7 +17,7 @@ test('flat_ini', () => {
 });
 
 test('nested_json', () => {
-  const compare = compareNested(`${fixturePath}/file3.json`, `${fixturePath}/file4.json`);
+  const compare = compareNested(`${fixturePath}/file1.json`, `${fixturePath}/file2.json`);
   const result = fs.readFileSync(`${fixturePath}/result3.txt`, { encoding: 'utf8', flag: 'r' });
   expect(compare).toEqual(result);
 });
