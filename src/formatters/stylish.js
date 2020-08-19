b023a08d3f28ecd4d16d8f7058fd0bdff7eb8eea
Alexey Buravov <night.lord.189@gmail.com>
2020-08-20 03:26:19 +0600

    formatter const rename

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,15 +1,15 @@
 import _ from 'lodash';
 
-const stringPrefix = '  ';
+const indent = '  ';
 
 const valueToString = (value, depth) => {
     if (!_.isObject(value)) {
         return value.toString();
     }
     const keysStr = Object.keys(value).map((key)=>{
-        return `${stringPrefix.repeat(depth * 3)}${key}: ${valueToString(value[key], depth+1)}`;
+        return `${indent.repeat(depth * 3)}${key}: ${valueToString(value[key], depth+1)}`;
     });
-    return `{\r\n${keysStr.join('\r\n')}\r\n${stringPrefix.repeat(depth * 2 - 1)}}`;
+    return `{\r\n${keysStr.join('\r\n')}\r\n${indent.repeat(depth * 2 - 1)}}`;
 }
 
 const format = (nodes, depth = 1) => {
@@ -17,23 +17,22 @@ const format = (nodes, depth = 1) => {
     const dataArrStr = sorted.map((node)=> {
         switch (node.status) {
             case 'same':
-              return `${stringPrefix.repeat(depth * 2)}${node.key}: ${valueToString(node.value, depth)}`;
+              return `${indent.repeat(depth * 2)}${node.key}: ${valueToString(node.value, depth)}`;
             case 'remove':
-              return `${stringPrefix.repeat(depth * 2 - 1)}- ${node.key}: ${valueToString(node.value, depth)}`;
+              return `${indent.repeat(depth * 2 - 1)}- ${node.key}: ${valueToString(node.value, depth)}`;
             case 'add':
-              return `${stringPrefix.repeat(depth * 2 - 1)}+ ${node.key}: ${valueToString(node.value, depth)}`;
+              return `${indent.repeat(depth * 2 - 1)}+ ${node.key}: ${valueToString(node.value, depth)}`;
             case 'change':
-              return `${stringPrefix.repeat(depth * 2 - 1)}- ${node.key}: ${valueToString(node.value, node.depth)}\r\n${stringPrefix.repeat(depth * 2 - 1)}+ ${node.key}: ${valueToString(node.valueNew, depth)}`;
+              return `${indent.repeat(depth * 2 - 1)}- ${node.key}: ${valueToString(node.value, node.depth)}\r\n${indent.repeat(depth * 2 - 1)}+ ${node.key}: ${valueToString(node.valueNew, depth)}`;
             case 'nested':
-              return `${stringPrefix.repeat(depth * 2)}${node.key}: ${format (node.children, depth + 1)}`;
+              return `${indent.repeat(depth * 2)}${node.key}: ${format (node.children, depth + 1)}`;
             default:
               return '';
           }
     });
     const dataStr = dataArrStr.join('\r\n');
-    const result = `{\r\n${dataStr}\r\n${stringPrefix.repeat(depth * 2 - 2)}}`;
+    const result = `{\r\n${dataStr}\r\n${indent.repeat(depth * 2 - 2)}}`;
     return result;
   };
   
   export default format;
-  
