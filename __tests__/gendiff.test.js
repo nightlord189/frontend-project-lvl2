ce94bbffba5ae39bd031de3ba6ee5430c0a3f4af
Alexey Buravov <night.lord.189@gmail.com>
2020-08-25 23:54:31 +0600

    YAML tests. gitignore change

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,12 +4,15 @@ import path from 'path';
 import genDiff from '../src/gendiff.js';
 
 const testData = [
-  ['file1.yml', 'file2.yml', 'stylish', 'result_flat.txt'],
-  ['file1.ini', 'file2.ini', 'stylish', 'result_nested.txt'],
   ['file1.json', 'file2.json', 'stylish', 'result_nested.txt'],
   ['file1.json', 'file2.json', 'plain', 'result_plain_format.txt'],
   ['file1.json', 'file2.json', 'json', 'result_json_format.txt'],
+  ['file1.yml', 'file2.yml', 'stylish', 'result_nested.txt'],
+  ['file1.yml', 'file2.yml', 'plain', 'result_plain_format.txt'],
+  ['file1.yml', 'file2.yml', 'json', 'result_json_format.txt'],
+  ['file1.ini', 'file2.ini', 'stylish', 'result_nested.txt'],
   ['file1.ini', 'file2.ini', 'json', 'result_json_format.txt'],
+  ['file1.ini', 'file2.ini', 'plain', 'result_plain_format.txt'],
 ];
 
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
