6dd7937f92eb6be8500ab9e0687d6208f30e2371
Alexey Buravov <night.lord.189@gmail.com>
2020-08-26 00:08:07 +0600

    bin file change

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,8 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .usage('[options] <filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish');
-
-program.version('1.0.0')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .version('1.0.0')
   .arguments('<file1> <file2>')
   .action((file1, file2, options) => {
     const result = genDiff(file1, file2, options.format);
