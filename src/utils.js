bc63480eadf8049be7737c44b1f07eaa9fc06417
Alexey Buravov <night.lord.189@gmail.com>
2020-08-31 15:06:25 +0600

    getAllKeys simplify

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const getAllKeys = (data1, data2) => {
-  const merged = { ...(_.isObject(data1) ? data1 : {}), ...(_.isObject(data2) ? data2 : {}) };
+  const merged = { ...data1, ...data2 };
   const result = Object.keys(merged);
   return result;
 };
