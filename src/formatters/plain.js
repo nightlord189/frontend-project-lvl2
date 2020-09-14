e8d85672549ebe9300927ec3ad11ded489796817
Alexey Buravov <night.lord.189@gmail.com>
2020-09-15 00:40:05 +0600

    Parsers refactor

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const valToString = (value) => {
+const stringifyValue = (value) => {
   if (!_.isObject(value)) {
     return _.isString(value) ? `'${value}'` : value;
   }
@@ -11,19 +11,17 @@ const getPropertyName = (property, parent) => (parent !== null ? `${parent}.${pr
 
 const format = (nodes, parent = null) => {
   const sorted = [...nodes].sort((a, b) => (a.key > b.key ? 1 : -1));
-  const stringified = sorted.map((node) => {
+  const stringified = sorted.filter(x=>x.status != 'same').map((node) => {
     const property = getPropertyName(node.key, parent);
     switch (node.status) {
       case 'removed':
         return `Property '${property}' was removed`;
       case 'added':
-        return `Property '${property}' was added with value: ${valToString(node.value)}`;
+        return `Property '${property}' was added with value: ${stringifyValue(node.value)}`;
       case 'changed':
-        return `Property '${property}' was updated. From ${valToString(node.value)} to ${valToString(node.valueNew)}`;
+        return `Property '${property}' was updated. From ${stringifyValue(node.value)} to ${stringifyValue(node.valueNew)}`;
       case 'nested':
         return format(node.children, property);
-      default:
-        return null;
     }
   });
   const result = stringified.filter((x) => !_.isNull(x)).join('\n');
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,19 +4,19 @@ const indent = '  ';
 
 const getIndent = (count) => indent.repeat(count);
 
-const valToString = (value, depth) => {
+const stringifyValue = (value, depth) => {
   if (!_.isObject(value)) {
     return value;
   }
-  const keysStr = Object.keys(value).map((key) => `${getIndent((depth + 1) * 2)}${key}: ${valToString(value[key], depth + 1)}`);
+  const keysStr = Object.keys(value).map((key) => `${getIndent((depth + 1) * 2)}${key}: ${stringifyValue(value[key], depth + 1)}`);
   return `{\n${keysStr.join('\n')}\n${getIndent(depth * 2)}}`;
 };
 
 const format = (nodes, depth = 1) => {
   const sorted = nodes.sort((a, b) => (a.key > b.key ? 1 : -1));
   const stringified = sorted.map((node) => {
-    const value = valToString(node.value, depth);
-    const valueNew = valToString(node.valueNew, depth);
+    const value = stringifyValue(node.value, depth);
+    const valueNew = stringifyValue(node.valueNew, depth);
     switch (node.status) {
       case 'same':
         return `${getIndent(depth * 2)}${node.key}: ${value}`;
@@ -28,8 +28,6 @@ const format = (nodes, depth = 1) => {
         return `${getIndent(depth * 2 - 1)}- ${node.key}: ${value}\n${getIndent(depth * 2 - 1)}+ ${node.key}: ${valueNew}`;
       case 'nested':
         return `${getIndent(depth * 2)}${node.key}: ${format(node.children, depth + 1)}`;
-      default:
-        return null;
     }
   }).join('\n');
   const result = `{\n${stringified}\n${getIndent(depth * 2 - 2)}}`;
