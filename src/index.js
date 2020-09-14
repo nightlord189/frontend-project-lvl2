73dcc9c995f55571084da9fcba1fb9241087c3d7
Alexey Buravov <night.lord.189@gmail.com>
2020-09-15 01:15:55 +0600

    index.js refactor

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import parse from './parsers.js';
 import buildAST from './ast.js';
 import format from './formatters/index.js';
 
+const parseFile = (filePath) => {
+  const fileData = fs.readFileSync(filePath, 'utf8');
+  return parse(fileData, path.extname(filePath).replace('.', ''));
+}
+
 const genDiff = (filePathBefore, filePathAfter, formatType = 'stylish') => {
-  const fileDataBefore = fs.readFileSync(filePathBefore, 'utf8');
-  const fileDataAfter = fs.readFileSync(filePathAfter, 'utf8');
-  const parsedBefore = parse(fileDataBefore, path.extname(filePathBefore).replace('.', ''));
-  const parsedAfter = parse(fileDataAfter, path.extname(filePathAfter).replace('.', ''));
+  const parsedBefore = parseFile(filePathBefore);
+  const parsedAfter = parseFile(filePathAfter);
   const ast = buildAST(parsedBefore, parsedAfter);
   return format(ast, formatType);
 };
